f811655d2b4e11862958645d6e030b2c5d1ecc49
Rowan Flood <121908273+rflood07@users.noreply.github.com>
2025-01-24 15:51:54 -0800

    don't stop the elevator after elevator command, so that it will continue going to it's position, even after the command ends

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -53,7 +53,6 @@ public class ElevatorCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    elevator.stopElevator();
 
   }
 
