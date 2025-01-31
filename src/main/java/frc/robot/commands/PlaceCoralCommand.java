85671e24e12fc103e6c131988a08ed1c0e33e077
Programming7 <2491NoMythic@gmail.com>
2025-01-30 21:52:20 -0800

    created copy of placeCoralCommand without a path

--- src/main/java/frc/robot/commands/PlaceCoralCommand.java
+++ src/main/java/frc/robot/commands/PlaceCoralCommand.java
@@ -2,27 +2,16 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.ElevatorCommand;
-import frc.robot.commands.NamedCommands.DeliverCoral;
 import frc.robot.settings.ElevatorEnums;
 import frc.robot.subsystems.CoralEndeffectorSubsystem;
 import frc.robot.subsystems.DistanceSensors;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 import java.util.function.Supplier;
-import frc.robot.commands.NamedCommands.DeliverCoral;
-import frc.robot.commands.ApproachReef;
-import frc.robot.commands.LineUp;
 
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
-
-import com.pathplanner.lib.auto.NamedCommands;
 
 
 public class PlaceCoralCommand extends SequentialCommandGroup{
@@ -33,12 +22,7 @@ public class PlaceCoralCommand extends SequentialCommandGroup{
             CoralEndeffectorSubsystem coralEndeffector, BooleanSupplier leftPlace){
         addCommands(
             new PathFindToReef(drivetrain, leftPlace),
-            new ApproachReef(distanceSensors, drivetrain, xSupplier, ySupplier, rSupplier),//approaches reef while raising elevator
-            new ParallelRaceGroup(
-                new ElevatorCommand(elevator, elevatorPose),//raises elevator to position
-                new LineUp(drivetrain, leftPlace, 0.3)),//align with reef
-            new DeliverCoral(coralEndeffector),//drops coral
-            new InstantCommand(()->elevator.setElevatorPosition(ElevatorEnums.Bottom), elevator) //sets elevator back to the bottom position
+            new PlaceCoralNoPath(elevator, elevatorPose, distanceSensors, drivetrain, xSupplier, ySupplier, rSupplier, coralEndeffector, leftPlace)
         );
 
     }
