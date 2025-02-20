c7a26f10292291ed3803b8b4b94bbfbf26af535e
Serena <serenathurlow55@gmail.com>
2025-02-20 17:58:37 -0600

    made lineUpCoralNamedCommand use a ParallelRaceGroup to actually finish

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,6 +40,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -776,12 +777,13 @@ public class RobotContainer {
       elevatorResetNamedCommand = new InstantCommand(()->System.out.println("attempted to create named command but subsytem did not exist"));
     }
     if(coralEndeffectorExists) {
-      lineUpCoralNamedCommand = new LineupCoralInEndEffector(coralEndDefector);
+      lineUpCoralNamedCommand = new ParallelRaceGroup(new LineupCoralInEndEffector(coralEndDefector),
+          new WaitUntil(() -> RobotState.getInstance().coralAligned));
     } else {
       lineUpCoralNamedCommand = new InstantCommand(()->System.out.println("attempted to create named command but subsytem did not exist (attempted lineUpCoralInEndeffector)"));
     }
 
-
+  
     NamedCommands.registerCommand("LineUpCoralInEndeffector", lineUpCoralNamedCommand);
     NamedCommands.registerCommand("CoralIntake", coralIntakeNamedCommand);
     NamedCommands.registerCommand("DeliverCoralLeft1", deliverCoralLeft1NamedCommand);
