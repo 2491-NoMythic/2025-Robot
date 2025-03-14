d238b65520706b875ee4c803a2a1a4fde1056584
programming06 <2491nomythic@gmail.com>
2025-03-13 19:30:55 -0500

    Algae Steaking command group made

---
+++ src/main/java/frc/robot/commands/StealAlgaeSequential.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static frc.robot.settings.Constants.AlgaeEndeffectorConstants.ALGAE_INTAKE_SPEED;
+import static frc.robot.settings.Constants.ElevatorConstants.METERS_FROM_POSE_TO_RAISE_ELEVATOR;
+import static frc.robot.settings.Constants.Field.REEF_POSITION_THRESHOLD;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.AlgaeEndeffectorSubsystem;
+import frc.robot.subsystems.CoralEndeffectorSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.commands.DriveToPose;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import frc.robot.commands.NamedCommands.DeliverCoral;
+import frc.robot.settings.ElevatorEnums;
+import frc.robot.settings.PlacementLocations;
+import frc.robot.commands.WaitUntil;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import frc.robot.subsystems.RobotState;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class StealAlgaeSequential extends SequentialCommandGroup {
+  /** Creates a new PlaceCoralDuringLineupSequential. */
+  public StealAlgaeSequential(AlgaeEndeffectorSubsystem algaeEndDefector, DrivetrainSubsystem driveTrain, ElevatorSubsystem elevator, CoralEndeffectorSubsystem coralEndDefector, Supplier<PlacementLocations> placementSupplier) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new InstantCommand(()->RobotState.getInstance().reefLineupRunning = true),
+      new InstantCommand(()->algaeEndDefector.runAlgaeEndDefector(() -> RobotState.getInstance().goForAlgae ? ALGAE_INTAKE_SPEED : -0.7), algaeEndDefector),
+      new ParallelDeadlineGroup(
+        new SequentialCommandGroup(
+          new WaitCommand(0.1),
+          new WaitUntil(()->driveTrain.getPositionTargetingError() < METERS_FROM_POSE_TO_RAISE_ELEVATOR),
+          new InstantCommand(()->elevator.setElevatorPosition(ElevatorEnums.Reef4), elevator)
+        ))
+   );
+  }
+}
