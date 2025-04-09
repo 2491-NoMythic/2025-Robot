63c3d00d88607ae03688c99548223b6bab487605
KnightPiscesAugie <94499208+KnightPiscesAugie@users.noreply.github.com>
2025-04-08 19:17:08 -0500

    Going to wait for further unbork

--- src/main/java/frc/robot/subsystems/FunnelRotator.java
+++ src/main/java/frc/robot/subsystems/FunnelRotator.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.helpers.MotorLogger;
