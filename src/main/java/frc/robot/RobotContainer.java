96bd22f43968e505948dbbc61652cab36b5d1c55
programming06 <2491nomythic@gmail.com>
2025-04-10 19:35:57 -0500

    I think I have unborked things

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -363,8 +363,8 @@ public class RobotContainer {
       ForceEjectCoral = buttonBoard::getForceEjectCoralButton;
       algaeShooterSupOperator = buttonBoard::getForceEjectAlgaeButton;
 
-      climberGroupForward = buttonBoard::getclimbCommandButton;
-      climberGroupReverse = buttonBoard::getClimbModeAuthorizer;
+      climberGroupForward = buttonBoard::getStartClimbingButton;
+      climberGroupReverse = buttonBoard::getClimberDeployButton;
       L1Mode = buttonBoard::getL1ModeButton;
     }
     if (LimelightExists) {limelightInit();}
